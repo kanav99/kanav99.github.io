bb7381c8c91cde80f2c960f27508c0f8718cccc9
Kanav Gupta <kanav0610@gmail.com>
2024-12-15 16:11:50 -0500

    heic to png

--- src/App.jsx
+++ src/App.jsx
@@ -189,7 +189,7 @@ export const App = () => {
               </VStack>
             </VStack>
             <VStack width={["100%", "50%"]} align="center">
-              <Image src={`/me.heic`}/>
+              <Image src={`/me.png`}/>
               {/* <Text style={captionStyle}>{`${captions[idx]}`}</Text> */}
             </VStack>
           </Stack>
